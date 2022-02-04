e5edab072b92d314c4ec005d0220087f4f02ae80
BZhang-ucsd <boz002@ucsd.edu>
2022-02-03 17:38:35 -0800

    tester passes

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -1,10 +1,11 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-
 public class SkillDemoTester {
     @Test
     public void isPrime(){
-        assertTrue("18  is Prime",SkillDemo.isPrime(18));
+
+        //now we change the tester so it passes
+        assertTrue("17  is Prime",SkillDemo.isPrime(17));
     }
 }
