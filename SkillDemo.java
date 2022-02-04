43d76512f66587d9afa52dca9c903670a7d59319
BZhang-ucsd <boz002@ucsd.edu>
2022-02-03 17:35:52 -0800

    failed tester

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,24 @@
 public class SkillDemo{
-//edit later
+//I will pull code that I wrote to make the video shorter
+
+//check if the input number is a prime number
+static boolean isPrime(int input){
+
+    boolean isPrime = true;
+
+    if(input <= 1){
+        isPrime = false;
+        return isPrime;
+    }else{
+        for(int i = 2; i<= input/2;i++){
+            if((input%i)==0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
+    }
+}
+
 }
---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemoTester {
+    @Test
+    public void isPrime(){
+        assertTrue("18  is Prime",SkillDemo.isPrime(18));
+    }
+}
